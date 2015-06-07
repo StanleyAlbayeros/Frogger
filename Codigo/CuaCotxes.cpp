9b87c99e6013ccf979ed82aa55afb92afc04e160
drkztan <drkztan@gmail.com>
2015-06-07 17:20:40 +0200

    implementando colas

--- Codigo/CuaCotxes.cpp
+++ Codigo/CuaCotxes.cpp
@@ -2,10 +2,18 @@
 
 CuaCotxes::CuaCotxes(Grafic grafic, int velocitat, bool direccio)
 {
-
+	m_grafic = grafic;
+	m_velocitat = velocitat;
+	m_direccio = direccio;
+	//creamos la cola y una instancia de coche que copiariemos para cada nodo de la cola
+	m_cua=Cua();
+	m_cotxe = Cotxe(m_grafic, m_velocitat, m_direccio);
 }
+
+
 
 
 CuaCotxes::~CuaCotxes(void)
 {
+	m_cua.~Cua();
 }
--- Codigo/CuaCotxes.h
+++ Codigo/CuaCotxes.h
@@ -6,13 +6,18 @@ class CuaCotxes
 {
 public:
 	CuaCotxes(Grafic grafic, int velocitat, bool direccio);
+	bool getColisions(Area area);
+	void mouCua();
+	void dibuixaCua();
 	~CuaCotxes(void);
 
 private:
+	Cua m_cua;
+	Cotxe m_cotxe;
 	Grafic m_grafic;
 	bool m_direccio;
-	int m_posicioX;
-	int m_posicioY;
+	int m_posicioLiderX;
+	int m_posicioLiderY;
 	int m_velocitat;
 	int m_contador;
 };
